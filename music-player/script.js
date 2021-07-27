950d48b0379d8f8b3d8ae9c3dc469d467603ad80
Sabyasachi Sarkar <54597679+sabyasachi4943@users.noreply.github.com>
2021-07-28 00:18:14 +0600

    progress bar completed

--- music-player/script.js
+++ music-player/script.js
@@ -103,7 +103,6 @@ function updateProgressBar(e) {
     progress.style.width = `${progressPercent}%`;
       // Calculate display for duration
       const durationMinutes = Math.floor(duration / 60);
-      console.log('minutes', durationMinutes);
       let durationSeconds = Math.floor(duration % 60);
       if (durationSeconds < 10) {
         durationSeconds = `0${durationSeconds}`;
@@ -116,7 +115,6 @@ function updateProgressBar(e) {
     }
     // Calculate display for current
     const currentMinutes = Math.floor(currentTime / 60);
-    console.log('minutes', currentMinutes);
     let currentSeconds = Math.floor(currentTime % 60);
     if (currentSeconds < 10) {
       currentSeconds = `0${currentSeconds}`;
@@ -126,7 +124,17 @@ function updateProgressBar(e) {
   }
 }
 
+// Set Progress Bar
+function setProgressBar(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const { duration } = music;
+  music.currentTime = (clickX / width) * duration;
+}
+
 // Event Listeners
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
+music.addEventListener('ended', nextSong);
 music.addEventListener('timeupdate', updateProgressBar);
+progressContainer.addEventListener('click', setProgressBar);
