2cb454e91c1803e052d55b10cd353602402327b6
Sabyasachi Sarkar <54597679+sabyasachi4943@users.noreply.github.com>
2021-07-27 23:57:08 +0600

    progress bar 2

--- music-player/script.js
+++ music-player/script.js
@@ -4,6 +4,8 @@ const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
 const progressContainer = document.getElementById('progress-container');
 const progress = document.getElementById('progress');
+const currentTimeEl = document.getElementById('current-time');
+const durationEl = document.getElementById('duration');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
@@ -98,8 +100,29 @@ function updateProgressBar(e) {
     console.log(duration, currentTime);
     // Update progress bar width
     const progressPercent = (currentTime / duration) * 100;
-    // console.log(ProgressPercent);
     progress.style.width = `${progressPercent}%`;
+      // Calculate display for duration
+      const durationMinutes = Math.floor(duration / 60);
+      console.log('minutes', durationMinutes);
+      let durationSeconds = Math.floor(duration % 60);
+      if (durationSeconds < 10) {
+        durationSeconds = `0${durationSeconds}`;
+        durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
+      }
+      
+    // Delay switching duration Element to avoid NaN
+    if (durationSeconds) {
+      durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
+    }
+    // Calculate display for current
+    const currentMinutes = Math.floor(currentTime / 60);
+    console.log('minutes', currentMinutes);
+    let currentSeconds = Math.floor(currentTime % 60);
+    if (currentSeconds < 10) {
+      currentSeconds = `0${currentSeconds}`;
+      currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`;
+    }
+    
   }
 }
 
