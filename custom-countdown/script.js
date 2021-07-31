250f8be94ea0259d892afae470c578cca5020781
Sabyasachi Sarkar <54597679+sabyasachi4943@users.noreply.github.com>
2021-07-31 23:51:47 +0600

    Form values

--- custom-countdown/script.js
+++ custom-countdown/script.js
@@ -2,7 +2,21 @@ const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('countdownForm');
 const dateEl = document.getElementById('date-picker');
 
+let countdownTitle = '';
+let countdownDate = '';
+
 // Set date input min with today's date
 const today = new Date().toISOString().split('T')[0];
-console.log(today);
+// console.log(today);
 dateEl.setAttribute('min', today);
+
+// Take values from form input
+function updateCountdown(e) {
+  e.preventDefault();
+  countdownTitle = e.srcElement[0].value;
+  countdownDate = e.srcElement[1].value;
+  console.log(countdownTitle, countdownDate);
+}
+
+// Event listeners
+countdownForm.addEventListener('submit', updateCountdown);
